51308bae31b413024bc9308db37dd6260cdb5634
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-08 12:40:10 -0500

    Archivo donde se maneja la conexion a la base de datos y se registra a los controladores

---
+++ main.py
@@ -0,0 +1,49 @@
+# main.py
+#Importar Librerias
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import text
+
+# Importar la base de datos desde database.py
+from database import db
+
+#Importar controladores
+from controllers.auth_controller import auth_controller
+from controllers.dashboard_controller import dashboard_controller
+from controllers.main_controller import main_controller
+from models.users_model import User
+
+
+# Cargar variables de entorno
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = "DysonSlMilo2003"
+
+# Configurar SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Inicializar la base de datos con la aplicación
+db.init_app(app)
+
+# Probar la conexión sin crear tablas
+with app.app_context():
+    try:
+        db.session.execute(text("SELECT 1"))
+        print("Connection successful")
+    except OperationalError as e:
+        print("Connection failed:", e)
+
+# Registrar blueprints
+app.register_blueprint(main_controller)
+app.register_blueprint(auth_controller)
+app.register_blueprint(dashboard_controller)
+
+if __name__ == "__main__":
+    app.run(debug=True)
