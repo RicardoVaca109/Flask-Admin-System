44060984b02e1b080668d0920295d1d3a661a4bb
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-08 16:14:28 -0500

    Cambio para conectarse a una base de datos en Railway

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.secret_key = "DysonSlMilo2003"
 # Configurar SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
