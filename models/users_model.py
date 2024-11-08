0423abc27e74aaee01e384e9652ed4d7e2b4f079
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-08 12:48:43 -0500

    Modelo de Users que muestra como esta formada la tabla users en la base de datos

---
+++ models/users_model.py
@@ -0,0 +1,21 @@
+# users_model.py #
+from database import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    contrasenia = db.Column(db.String(300), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+
+    def set_password(self, password):
+        self.contrasenia = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.contrasenia, password)
+    
+    def set_email(self, email):
+        self.email = email
