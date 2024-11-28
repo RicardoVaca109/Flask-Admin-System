8284092912448600ea212e371f194aa02229e643
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-28 08:57:19 -0500

    Modelo Tabla SQL de Pacientes

---
+++ models/patients_model.py
@@ -0,0 +1,14 @@
+from database import db
+
+class Patient(db.Model):
+    __tablename__ = 'Pacientes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    edad = db.Column(db.Integer)
+    contacto = db.Column(db.String(100))
+    fecha_registro = db.Column(db.Date)
+
+    # # Relación con PacienteEnfermedad
+    enfermedades = db.relationship('PacienteEnfermedad', back_populates='paciente')
