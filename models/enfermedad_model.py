99896c7571cc72ba385ef43ec1db6da378f4d289
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-28 08:45:03 -0500

    Modelo de la tabla SQL Enfermedad

---
+++ models/enfermedad_model.py
@@ -0,0 +1,11 @@
+from database import db
+
+class Enfermedad(db.Model):
+    __tablename__ = 'Enfermedades'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(200), nullable=False)
+    descripcion = db.Column(db.String(250))
+
+    # Relación con PacienteEnfermedad
+    pacientes = db.relationship('PacienteEnfermedad', back_populates='enfermedad')
