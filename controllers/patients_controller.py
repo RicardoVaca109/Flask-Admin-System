25deccba1a427df83cfea57f66b48f326bff5764
Ricardo <ricardo.vaca@udla.edu.ec>
2024-11-28 08:44:27 -0500

    Controlador de Usuarios con rol Enfermeria. Manejo de Pacientes Cmabios Iniciales

---
+++ controllers/patients_controller.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, url_for, redirect, session, request, flash
+from sqlalchemy.orm import joinedload
+from database import db
+#Al importar los modelos de las tablas hacerlos por orden para que funcionen al 100%
+from models.patients_model import Patient
+from models.enfermedad_model import Enfermedad
+from models.patientenfermedad_model import PacienteEnfermedad
+
+patients_controller = Blueprint("patients_controller", __name__)
+
+@patients_controller.route("/manage-patients")
+def manage_patients():
+    if "username" in session and session ["role_id"] == 4:
+        #pacientes = Patient.query.all()
+        pacientes = Patient.query.options(joinedload(Patient.enfermedades).joinedload(PacienteEnfermedad.enfermedad))
+        return render_template("manage_patients.html", pacientes = pacientes)
+    return redirect(url_for("main_controller.index"))
