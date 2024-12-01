c48de3ab7fab374287ef0dc256f9e64707314eb6
Ricardo <ricardo.vaca@udla.edu.ec>
2024-12-01 14:35:26 -0500

    Cambios menores

--- models/patientenfermedad_model.py
+++ models/patientenfermedad_model.py
@@ -1,11 +1,12 @@
 from database import db
 
-class PacienteEnfermedad(db.Model):
+class PatienteEnfermedad(db.Model):
     __tablename__ = 'Paciente_Enfermedad'
 
     id_paciente = db.Column(db.Integer, db.ForeignKey('Pacientes.id'), primary_key=True, nullable=False)
     id_enfermedad = db.Column(db.Integer, db.ForeignKey('Enfermedades.id'), primary_key=True, nullable=False)
     
-    # Relación con Tabla Paciente (patients_model.py) y Tabla Enfermedad (enfermedad_model.py)
+    # Relación con Tabla Paciente (patients_model.py) 
     paciente = db.relationship('Patient', back_populates='enfermedades')
+    # Relacion Tabla Enfermedad (enfermedad_model.py)
     enfermedad = db.relationship('Enfermedad', back_populates='pacientes')
