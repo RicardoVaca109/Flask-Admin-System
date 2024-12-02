28f6d61611899c965b99f3a6ebd7ec452b274ca9
Ricardo <ricardo.vaca@udla.edu.ec>
2024-12-01 19:22:44 -0500

    Cambios en el modelo Paciente para que contacto sean correos y podamos también verificar el correo

--- models/patients_model.py
+++ models/patients_model.py
@@ -1,5 +1,5 @@
 from database import db
-
+from email_validator import validate_email, EmailNotValidError
 class Patient(db.Model):
     __tablename__ = 'Pacientes'
 
@@ -7,10 +7,17 @@ class Patient(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     edad = db.Column(db.Integer)
-    contacto = db.Column(db.String(100))
+    contacto = db.Column(db.String(100), unique=True, nullable=False)
     fecha_registro = db.Column(db.Date)
 
     # Relación con Tabla PatienteEnfermedad (patientenfermedad_model.py)
     enfermedades = db.relationship('PatienteEnfermedad', back_populates='paciente')
     #  Relación con Tabla Consumo (consumos_model.py)
     consumos = db.relationship("Consumo", back_populates="paciente")
+
+    def set_contact(self, contacto):
+        try:
+            valid_email = validate_email(contacto).email
+            self.contacto = valid_email
+        except EmailNotValidError as e:
+            raise ValueError(f"Email inválido: {str(e)}")
