8b8b5353760edebadb8501cac48f61292bc31745
Ricardo <ricardo.vaca@udla.edu.ec>
2025-01-28 09:59:07 -0500

    Patrón singleton explicitamente

--- database.py
+++ database.py
@@ -1,4 +1,18 @@
-# database.py
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+            cls._instance.db = SQLAlchemy()  # Crear la instancia única de SQLAlchemy
+        return cls._instance
+
+    @staticmethod
+    def get_db():
+        return DatabaseConnection()._instance.db
+
+
+# Crear una función para obtener la instancia de la base de datos
+db = DatabaseConnection.get_db()
